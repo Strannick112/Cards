d8879676f8772c0aff0b6dc865f8a05d82621deb
Stanislav <stas960@gmail.com>
2024-02-29 15:23:19 +0300

    Добавление колоды

--- game/Deck.c
+++ game/Deck.c
@@ -3,3 +3,23 @@
 //
 
 #include "Deck.h"
+
+Card* get_card(struct Deck* deck){
+    if(deck->_index < 36)
+        return deck->_deck[deck->_index++];
+    else{
+        printf("Карты кончились!\n");
+        exit(-1);
+    }
+}
+
+void sort(struct Deck* deck){
+    for(int i = 0; i < 100; i++){
+        int first_rand = rand() % 36;
+        int second_rand = rand() % 36;
+        Card* card = deck->_deck[first_rand];
+        deck->_deck[first_rand] = deck->_deck[second_rand];
+        deck->_deck[second_rand] = card;
+    }
+    deck->_index = 0;
+}
--- game/Deck.h
+++ game/Deck.h
@@ -5,4 +5,33 @@
 #ifndef UNTITLED33_DECK_H
 #define UNTITLED33_DECK_H
 
+#include "Card.h"
+
+typedef struct Deck{
+    Card* _deck[36];
+    int _index;
+
+    Card* (*get_card)(struct Deck* deck);
+    void (*sort)(struct Deck* deck);
+} Deck;
+
+Card* get_card(struct Deck* deck);
+void sort(struct Deck* deck);
+
+Deck* deck_init(){
+    Deck* result = malloc(sizeof(Deck));
+    for(int mast_index = 0; mast_index < 4; mast_index++){
+        for(int rang_index = 0; rang_index < 9; rang_index++){
+            result->_deck[mast_index * 9 + rang_index] = Card(
+                    masts[mast_index], rangs[rang_index]
+            );
+        }
+    }
+
+    result->sort = sort;
+    result->get_card = get_card;
+    result->sort(result);
+    return result;
+}
+
 #endif //UNTITLED33_DECK_H
